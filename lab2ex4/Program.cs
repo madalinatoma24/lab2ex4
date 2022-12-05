3def5835c441d570fbc1f29184bb91cbcddc57af
madalina.toma <madalina.toma@softem.ro>
2022-12-05 22:55:47 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ex 4: Scrieti un program care va determina daca un numar este sau nu palindrom.
+            */
+
+            Console.WriteLine("Introduceti numarul: ");
+            int number = int.Parse(Console.ReadLine());
+            int rest = 0;
+            int sum = 0;
+            int temp = number;
+
+            while (number > 0)
+            {
+                rest = number % 10;
+                sum = sum * 10 + rest;
+                number = number / 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine($"{temp} este palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"{temp} nu este palindrom");
+            }
+
+        }
+    }
+}
